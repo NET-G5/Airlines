3048b6dc5603021976ea38c4b91add1f8eb802f9
begzhanmaxamatxanov <begzhan.maxamatxanov@gmail.com>
2024-09-01 19:38:32 +0500

    Update UI
    _Layaut Flight index and details

--- WEB/Airline.WEB/ViewModels/Flight/FlightView.cs
+++ WEB/Airline.WEB/ViewModels/Flight/FlightView.cs
@@ -4,8 +4,10 @@ public class FlightView
 {
     public int ID { get; set; }
     public string FlightNumber { get; set; }
-    public string DepartureAirport { get; set; }
-    public string ArrivalAirport { get; set; }
+    public string DepartureAirportCountry { get; set; }
+    public string DepartureAirportCity { get; set; }
+    public string ArrivalAirportCountry { get; set; }
+    public string ArrivalAirportCity { get; set; }
     public DateTime DepartureTime { get; set; }
     public DateTime ArrivalTime { get; set; }
     public decimal Price { get; set; }
