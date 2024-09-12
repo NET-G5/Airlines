8c468fc5a649abdf11785e996545c07fc5c62f24
Ramadan1011 <shukhratovich75@gmail.com>
2024-09-12 18:32:04 +0500

    removed connection from cofiguration default

--- WEB/Airline.Infrastructure/Persistence/Configurations/ConfigurationDefaults.cs
+++ WEB/Airline.Infrastructure/Persistence/Configurations/ConfigurationDefaults.cs
@@ -1,6 +1,6 @@
 namespace Airline.Infrastructure.Persistence.Configurations;
 
-public static class ConfigurationDefaults
-{
-    public const string ConectionString = "Server=localhost; Database=AirlineDataBase; User Id=sa; Password=MyP@ssw0rd123; TrustServerCertificate=True;";
-}
+//public static class ConfigurationDefaults
+//{
+//    public const string ConectionString = "Data Source=desktop-vvif9hi\\ramadan;Initial Catalog=airline;Integrated Security=True;Trust Server Certificate=True";
+//}
