e009a4de3229e6275b6c5a5377c7b4c1199f1536
begzhanmaxamatxanov <begzhan.maxamatxanov@gmail.com>
2024-10-02 19:25:34 +0500

    Delete AuditableEntity.cs

--- WEB/Airline.Domain/Common/AuditableEntity.cs
+++
@@ -1,7 +0,0 @@
-namespace Airline.Domain.Common;
-
-public abstract class AuditableEntity : EntityBase
-{
-    public DateTime CreatedAt { get; set; }
-    public DateTime? UpdatedAt { get; set; }
-}
