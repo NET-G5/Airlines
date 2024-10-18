4e6f57c72bbf4325fd1894ed367096e993bdad2b
begzhanmaxamatxanov <begzhan.maxamatxanov@gmail.com>
2024-10-19 00:15:16 +0500

    Delete UserRoleRepository.cs

--- WEB/Airline.Infrastructure/Repositories/UserRoleRepository.cs
+++
@@ -1,9 +0,0 @@
-using Airline.Domain.Entities;
-using Airline.Domain.Interfaces;
-
-namespace Airline.Infrastructure.Repositories;
-
-public class UserRoleRepository : RepositoryBase<UserRole>, IUserRoleRepository
-{
-    public UserRoleRepository(AirlineDbContext context) : base(context) {}
-}
