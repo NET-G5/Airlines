0e73f2ffa382f0ff47b135f3e20013e13fbca3fe
begzhanmaxamatxanov <begzhan.maxamatxanov@gmail.com>
2024-10-19 00:12:08 +0500

    Update

--- WEB/Airline.Domain/Common/EntityBase.cs
+++ WEB/Airline.Domain/Common/EntityBase.cs
@@ -2,7 +2,7 @@ namespace Airline.Domain.Common;
 
 public abstract class EntityBase
 {
-    public int ID { get; set; }
+    public int Id { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime? UpdatedAt { get; set; }
 }
